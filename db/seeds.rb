2cf515a7e82a3c2b5effe141ccea85337e6967cc
Jabernathy88 <j.e.abernathy@gmail.com>
2018-04-11 13:30:03 -0400

    Shifting directions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.new(
+  name: "1000 Piece Jigsaw Puzzle", release_date: "2013-04-27", price: 0.1499e2, created_at: "2018-04-11 16:57:59", updated_at: "2018-04-11 16:57:59"
+)
+
