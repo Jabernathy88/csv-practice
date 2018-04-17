a5f23bff5f58306546d9e88c575bd7911719630e
Jabernathy88 <j.e.abernathy@gmail.com>
2018-04-17 09:22:36 -0400

    Some progress. Still working on adding new CSV column.

--- script_practice/patch_it.rb
+++ script_practice/patch_it.rb
@@ -2,15 +2,26 @@
 
 require 'csv'
 
-test_table = CSV.read('./src/diabetes.csv', :headers => true)
+old_table = CSV.read('./src/diabetes.csv', :headers => true, converters: :numeric)
+old_table.delete("Triceps skin fold thickness (mm)")
 
-test_table.delete("Triceps skin fold thickness (mm)")
+new_header_array = ["transaction_id", "Number of times pregnant", "Plasma glucose concentration", "Diastolic blood pressure (mm Hg)", "2-Hour serum insulin (mu U/ml)", "Body mass index (weight in kg/(height in m)^2)", "Diabetes pedigree function", "Age (years)", "Class", "Age (days)"]
+age_in_years = old_table["Age (years)"]
+age_in_days = Array.new 
+
+old_table.select do |row|
+  age_in_days << (row["Age (years)"].to_i * 365).to_s
+end 
+
+CSV.foreach('./src/diabetes.csv', converters: :numeric) do |row|
+  age_in_days << age_in_years * 365
+end 
 
 CSV.open('./output.csv', 'w') do |new_table|
-  new_header_array = ["transaction_id", "Number of times pregnant", "Plasma glucose concentration", "Diastolic blood pressure (mm Hg)", "2-Hour serum insulin (mu U/ml)", "Body mass index (weight in kg/(height in m)^2)", "Diabetes pedigree function", "Age (years)", "Class"]
   new_table << new_header_array
-  test_table.each do |old_row|
-    new_table.puts old_row    
+  old_table.each do |old_row|
+    new_table.puts old_row
+    # new_table[-1].puts age_in_days
   end 
 end 
 
--- script_practice/src/drop_column.rb
+++
@@ -1,5 +0,0 @@
-require 'CSV'
-
-csv_table = CSV.read("diabetes.csv", :headers => true)
-csv_table.delete("Triceps skin fold thickness (mm)")
-csv_table.to_csv
